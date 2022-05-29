0b6291e3f867fda6b1972e449cd10b3fc24ed716
borhan <mdborhanuddinab21@gmail.com>
2022-05-30 01:22:46 +0600

    change bitcoin account number

--- bitcoin/settings.py
+++ bitcoin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m)e^z&6yp7b818_klbn-o6xnq1v8)4c!(_$c14)*+yk7r+ujcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["127.0.0.1"]
 ALLOWED_HOSTS = ["btcdouble.net","https://www.btcdouble.net", "www.btcdouble.net", "http://www.btcdouble.net"]
